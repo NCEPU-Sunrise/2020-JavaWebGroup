a323e7a173bc8aadfef42739573f7b96b3decb5a
lei324 <3242369851@qq.com>
2021-03-10 08:36:09 +0800

    Commit Four

---
+++ HuangLei_Homework/Work_4.java
@@ -0,0 +1,12 @@
+package Learning;
+
+public class Work_4 {
+    public static void main(String[] args){
+        String str=new String("hello WORD");
+        String newstr=str.toLowerCase();
+        String newstr1=str.toUpperCase();
+        System.out.println(newstr);
+        System.out.println(newstr1);
+    }
+
+}
